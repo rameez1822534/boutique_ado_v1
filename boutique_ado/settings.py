82ce79130f03de56ed7fefa1eb29542ec79f0f59
rameez1822534 <ch_rameez89@hotmail.com>
2020-08-22 22:15:26 +0000

    added homepage content

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -75,7 +75,6 @@ TEMPLATES = [
     },
 ]
 AUTHENTICATION_BACKENDS = [
- 
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
